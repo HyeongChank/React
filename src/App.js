706ab3e4c13a61a9a92bae82636e24887578b3c0
HyeongChank <jsk381988@gmail.com>
2023-02-03 13:55:45 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 
+import Taccident from './05/Taccident';
 
 // import Mydiv from './03/Mydiv';
-import Frcst from './04/Frcst';
-import './04/Frcst.css';
+// import Frcst from './04/Frcst';
+// import './04/Frcst.css';
 const App = () => {
   return (
 
     <div>
       {/* <Mydiv/> */}
-      <Frcst />
+      {/* <Frcst /> */}
+      {<Taccident/>}
 
     </div>
     
